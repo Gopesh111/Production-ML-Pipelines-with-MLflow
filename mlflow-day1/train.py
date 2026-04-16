fb238650ef0193c868620558c2da9cd71681ecc8
Gopesh Pandey <gopeshpandey0807@gmail.com>
2026-04-16 19:45:41 +0530

    Create train.py

---
+++ mlflow-day1/train.py
@@ -0,0 +1,40 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load data
+X, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Start MLflow run
+with mlflow.start_run():
+
+    n_estimators = 100
+    max_depth = 5
+
+    # Log parameters
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+
+    # Train model
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth
+    )
+    model.fit(X_train, y_train)
+
+    # Predictions
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    # Log metric
+    mlflow.log_metric("accuracy", acc)
+
+    # Log model
+    mlflow.sklearn.log_model(model, "model")
+
+    print(f"Accuracy: {acc}")
